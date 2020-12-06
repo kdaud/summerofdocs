7b5e2b55c4bc08914b878e36c6f090ccf732817e
kdaud <dkakumirizii@gmail.com>
2020-12-06 14:11:29 +0300

    Google api

---
+++ mycode/login.js
@@ -0,0 +1,2 @@
+document.write('Software Engineering is about managing software systems and supporting its revolution')
+document.write("<br>Programmers of tommorow will be the wizzard of the future")
